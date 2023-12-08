3641433b1d016cb5e0ba3c0aa89b6264b8c517fd
pranav <katkarpranav4@gmail.com>
2023-12-08 19:53:05 +0530

    fetched data live from swiggi api

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,9 +1,22 @@
 import RestaurantCard from "./RestaurantCard";
-import { resList } from "../utils/mockData";
 import { useState, useEffect } from "react";
 
 const Body = () => {
-  const [restaurantList, setRestaurantList] = useState(resList);
+  const [restaurantList, setRestaurantList] = useState([]);
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const response = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=15.2832187&lng=73.98619099999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const data = await response.json();
+    setRestaurantList(
+      data?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
+
   return (
     <div className="body">
       <div className="filter">
