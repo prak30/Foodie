4d3533a5eb78b4775c903fb323122c45654aa041
pranav <katkarpranav4@gmail.com>
2023-12-08 19:05:50 +0530

    Added functionality to filter data according to top rated by clicking top rated button

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,11 +1,26 @@
 import RestaurantCard from "./RestaurantCard";
 import { resList } from "../utils/mockData";
+import { useState, useEffect } from "react";
+
 const Body = () => {
+  const [restaurantList, setRestaurantList] = useState(resList);
   return (
     <div className="body">
-      <div className="search">Search</div>
+      <div className="filter">
+        <button
+          className="toprated"
+          onClick={() => {
+            const filteredList = restaurantList.filter(
+              (res) => res.info.avgRating > 4
+            );
+            setRestaurantList(filteredList);
+          }}
+        >
+          Top Rated
+        </button>
+      </div>
       <div className="res-container">
-        {resList.map((restaurant, index) => (
+        {restaurantList.map((restaurant, index) => (
           <RestaurantCard key={index} resdata={restaurant} />
         ))}
       </div>
