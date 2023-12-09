c85d6cc5a35dc62e3a6e7b54f5608e324e84aeee
pranav <katkarpranav4@gmail.com>
2023-12-09 11:15:51 +0530

    added shimmer to improve UX

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,8 +1,8 @@
 import RestaurantCard from "./RestaurantCard";
 import { useState, useEffect } from "react";
-
+import Shimmer from "./Shimmer";
 const Body = () => {
-  const [restaurantList, setRestaurantList] = useState([]);
+  const [listOfRestaurants, setListOfRestaurants] = useState([]);
   useEffect(() => {
     fetchData();
   }, []);
@@ -12,28 +12,32 @@ const Body = () => {
       "https://www.swiggy.com/dapi/restaurants/list/v5?lat=15.2832187&lng=73.98619099999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
     );
     const data = await response.json();
-    setRestaurantList(
-      data?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    console.log(data);
+    setListOfRestaurants(
+      data.data.cards[1].card.card.gridElements.infoWithStyle.restaurants
     );
   };
 
-  return (
+  return listOfRestaurants.length === 0 ? (
+    <Shimmer />
+  ) : (
     <div className="body">
       <div className="filter">
         <button
           className="toprated"
           onClick={() => {
-            const filteredList = restaurantList.filter(
+            const filteredList = listOfRestaurants.filter(
               (res) => res.info.avgRating > 4
             );
-            setRestaurantList(filteredList);
+            setListOfRestaurants(filteredList);
           }}
         >
           Top Rated
         </button>
       </div>
       <div className="res-container">
-        {restaurantList.map((restaurant, index) => (
+        {console.log(listOfRestaurants)}
+        {listOfRestaurants.map((restaurant, index) => (
           <RestaurantCard key={index} resdata={restaurant} />
         ))}
       </div>
---
+++ src/components/Shimmer.js
@@ -0,0 +1,18 @@
+const Shimmer = () => {
+  return (
+    <div className="shimcontainer">
+      <div className="shimelement"></div>
+      <div className="shimelement"></div>
+      <div className="shimelement"></div>
+      <div className="shimelement"></div>
+      <div className="shimelement"></div>
+      <div className="shimelement"></div>
+      <div className="shimelement"></div>
+      <div className="shimelement"></div>
+      <div className="shimelement"></div>
+      <div className="shimelement"></div>
+    </div>
+  );
+};
+
+export default Shimmer;
