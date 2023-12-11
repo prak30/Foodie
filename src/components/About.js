2d2852b2faa5c8f2a0955cf6c6fca87387f76ed0
pranav <katkarpranav4@gmail.com>
2023-12-11 12:09:52 +0530

    Added routes

---
+++ src/components/About.js
@@ -0,0 +1,51 @@
+const About = () => {
+  return (
+    <div className="about-us-container">
+      <h2>About Foodie</h2>
+      <p>
+        Welcome to Foodie, your go-to food delivery app for delicious meals
+        delivered right to your doorstep!
+      </p>
+
+      <h3>Our Mission</h3>
+      <p>
+        At Foodie, we are passionate about connecting people with their favorite
+        food from the best local restaurants. Our mission is to make food
+        delivery convenient, reliable, and delightful for everyone.
+      </p>
+
+      <h3>Why Choose Foodie?</h3>
+      <ul>
+        <li>
+          Wide Variety: Explore a diverse range of cuisines and dishes from
+          top-rated restaurants.
+        </li>
+        <li>
+          Fast Delivery: We prioritize timely deliveries to ensure your food is
+          fresh and satisfying.
+        </li>
+        <li>
+          User-Friendly App: Our app is designed to be easy to use, making
+          ordering your favorite meals a breeze.
+        </li>
+        <li>
+          Exceptional Service: Our dedicated customer support team is here to
+          assist you every step of the way.
+        </li>
+      </ul>
+
+      <h3>Contact Us</h3>
+      <p>
+        Have questions or feedback? We'd love to hear from you! Contact our
+        support team at support@foodieapp.com.
+      </p>
+
+      <p>
+        Thank you for choosing Foodie. We look forward to serving you delicious
+        meals!
+      </p>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/components/Contact.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div className="contact-us-container">
+      <h2>Contact Us</h2>
+      <p>
+        Thank you for your interest in our food delivery service. If you have
+        any questions or inquiries, feel free to reach out to us using the
+        contact information below.
+      </p>
+
+      <div className="contact-info">
+        <p>Email: info@foodieapp.com</p>
+        <p>Phone: +1 (123) 456-7890</p>
+        <p>Address: 123 Main Street, Cityville, State, 12345</p>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
