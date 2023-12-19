e0178b95b0022a4c32e8f3f818dc0c7091507b6a
pranav <katkarpranav4@gmail.com>
2023-12-20 03:30:35 +0530

    added ₹ symbol

--- src/components/ItemCard.js
+++ src/components/ItemCard.js
@@ -9,22 +9,22 @@ const ItemCard = ({ data }) => {
           key={item.card.info.id}
           className="text-left p-2 m-2 border-gray-200 border-b-2 flex justify-between "
         >
-          <div>
-            <div>
-              <span>{item.card.info.name}</span>
-              <span>- Rs {item.card.info.price}</span>
+          <div className="w-9/12">
+            <div className="py-2">
+              <span className="font-bold">{item.card.info.name}</span>
+              <span className="font-bold">- ₹{item.card.info.price}</span>
             </div>
             <div>
               <p>{item.card.info.description}</p>
             </div>
           </div>
-          <div className="p-4">
+          <div className="w-3/12 p-4">
             <img
               src={
                 "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" +
                 item.card.info.imageId
               }
-              className="w-48 h-48"
+              className="w-30 h-30"
             />
           </div>
         </div>
