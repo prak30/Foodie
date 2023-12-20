b94a629e39148005055e0a70aebe509a90608dc7
pranav <katkarpranav4@gmail.com>
2023-12-20 16:46:17 +0530

    Built a basic accordion

--- src/components/RestaurantCategory.js
+++ src/components/RestaurantCategory.js
@@ -1,20 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import ItemCard from "./ItemCard";
 
 const RestaurantCategory = ({ data }) => {
-  console.log(data);
+  const [showItems, setShowItems] = useState(false);
+  const handleClick = () => {
+    setShowItems(!showItems);
+  };
   return (
     <div className="w-6/12  mx-auto shadow-lg p-4">
-      <div className="flex justify-between">
+      <div
+        className="flex justify-between cursor-pointer"
+        onClick={handleClick}
+      >
         <span className="font-bold">
           {data.title} ({data.itemCards.length})
         </span>
         <span>⬇️</span>
       </div>
 
-      <div>
-        <ItemCard data={data.itemCards} />
-      </div>
+      <div>{showItems && <ItemCard data={data.itemCards} />}</div>
     </div>
   );
 };
